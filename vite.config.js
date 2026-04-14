fb2de30d1b5d07ee5ea3832d9fe50d2a0b53ce2f
MohamedElgendy71 <mohamedelgendy200421@gmail.com>
2026-04-14 02:01:16 +0200

    fix: blank page on refresh and routing

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react-social-app/',
+  base: '/',
   plugins: [react() , tailwindcss()],
 })
